e3ba62462b58b8e2cab0fed0a7688ba90fe286e0
Jake <15jps1@queensu.ca>
2020-03-12 22:19:42 -0400

    query 3

--- query3.php
+++ query3.php
@@ -14,15 +14,34 @@
         <h2 class="dashboard-title">ACTIONS</h1>
         <a href="query1.php"><button class="custom-button"><h4>Display Animal Information</h4></button></a></br>
         <a href="query2.html"><button class="custom-button"><h4>Transfer an Animal</h4></button></a></br>
-        <a href="query3.html"><button class="custom-button"><h4>Get Driver Information</h4></button></a></br>
+        <a href="query3.php"><button class="custom-button"><h4>Get Driver Information</h4></button></a></br>
         <a href="query4.php"><button class="custom-button"><h4>Get Donor Information</h4></button></a></br>
         <a href="query5.php"><button class="custom-button"><h4>Get Donation Amount</h4></button></a></br>
         <a href="query6.php"><button class="custom-button"><h4>Adopt a Pet</h4></button></a></br>
         <a href="query7.php"><button class="custom-button"><h4>Display Special Animals</h4></button></a></br>
         <a href="query8.php"><button class="custom-button"><h4>Display Number of Rescued Animals</h4></button></a></br>
       </div>
-      <div class="col-9">
-        
+      <div class="col-9" id="content">
+        <table class="custom-table">
+            <tr><th>Driver Name</th><th>Telephone Number</th><th>License Number</th><th>Plate Number</th><th>Rescue Organization</th></tr>
+            <?php
+                $organization_id = $_POST["organization_id"];
+                $dbh = new PDO('mysql:h ost=localhost;dbname=animal_database', "root", "");
+                $rescue_organizations = $dbh->query("select count(telephone_number) from rescue_organization where telephone_number = '$organization_id'");
+                foreach($rescue_organizations as $rescue_organization) {
+                if ($rescue_organization[0] > 0) {
+                    $drivers_info = $dbh->query("select * from driver where rescue_organization = '$organization_id'");
+                    foreach($drivers_info as $driver) {
+                        echo "<tr><td>".$driver[0]."</td><td>".$driver[1]."</td><td>".$driver[2]."</td><td>".$driver[3]."</td><td>".$driver[4]."</td></tr>";
+                    }
+                }
+                else {
+                    echo "<p>You have entered an invalid Rescue Organization ID.</p>";
+                }
+                }
+                $dbh = null;
+            ?>
+        </table>
       </div>
     </div>
   </body>
